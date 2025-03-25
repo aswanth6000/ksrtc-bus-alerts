2339d96aa541a1fcdc9b5fae8c76557de43591f0
aswanth6000 <aswanth682@gmail.com>
2025-03-25 22:00:35 +0530

    bug: cors err fix

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://ksrtc-bus-alerts.vercel.app/",
+  origin: "https://ksrtc-bus-alerts.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
